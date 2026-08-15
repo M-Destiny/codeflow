4e91375d160917b1aa61fd51a133aed2555389e6
Mehul <mehulr2801@gmail.com>
2026-08-15 16:25:52 +0000

    chore: monitor agent auto-commit at 20260815T162552Z

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ export default function App() {
           return prev;
         });
       });
-      return () => s.disconnect();
+      return () => { s.disconnect(); };
     }
   }, [joined]);
 
